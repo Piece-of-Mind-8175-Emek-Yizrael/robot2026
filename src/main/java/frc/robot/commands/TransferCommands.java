0719715bd01df97898e370ccb7d31f53a8008383
John <yojobama@gmail.com>
2026-01-24 19:18:10 +0200

    adding two non-required commands to a new file

---
+++ src/main/java/frc/robot/commands/TransferCommands.java
@@ -0,0 +1,15 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.transfer.Transfer;
+
+public class TransferCommands {
+    public static Command setVoltage(Transfer subsystem, double voltage) {
+        return Commands.runOnce(() -> subsystem.getIO().setVoltage(voltage));
+    }
+
+    public static Command stopMotor(Transfer subsystem) {
+        return Commands.runOnce(() -> subsystem.getIO().setVoltage(0));
+    }
+}
