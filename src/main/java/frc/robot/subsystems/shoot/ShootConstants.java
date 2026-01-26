b95be26f24cb80f50d692587cfadc4d358062088
gil-liGrota <gilgrota@gmail.com>
2026-01-26 18:17:26 +0200

    add pid need to chake

--- src/main/java/frc/robot/subsystems/shoot/ShootConstants.java
+++ src/main/java/frc/robot/subsystems/shoot/ShootConstants.java
@@ -11,4 +11,10 @@ public class ShootConstants {
     public static final int transferCurrentLimit = 10; //Amps
     public static final double transferEncoderPositionFactor = 1.0; //rotations
     public static final double transferEncoderVelocityFactor = transferEncoderPositionFactor / 60.0; //rotations per second
+    
+    public static final double kv = 1.0;
+    public static final double ks = 1.0;
+    public static final double kp = 1.0;
+    public static final double ki = 1.0;
+    public static final double kd = 1.0;
 }
