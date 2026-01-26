4339f46e4109ca4ee62a894e890207c0281b38c5
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2026-01-26 12:10:19 +0200

    downgraded the code (removed pid) and added another switch

--- src/main/java/frc/robot/subsystems/cartridge/cartridgeConstants.java
+++ src/main/java/frc/robot/subsystems/cartridge/cartridgeConstants.java
@@ -2,17 +2,17 @@ package frc.robot.subsystems.cartridge;
 
 public class cartridgeConstants {
     public static final int MOTOR_ID = 0;
-    public static final int LIMIT_SWITCH_CHANNEL = 0;
-    public static final boolean IS_NORMALLY_OPEN = false;
+    public static final int INNER_SWITCH_CHANNEL = 0;
+    public static final int OUTTER_SWITCH_CHANNEL = 0;
+    public static final boolean INNER_NORMALLY_OPEN = false;
+    public static final boolean OUTTER_NORMALLY_OPEN = false;
     public static final int kP = 0;
     public static final int kI = 0;
     public static final int kD = 0;
-    public static final int kV = 0;
-    public static final int kS = 0;
-    public static final double OPEN_POS = 0;
-    public static final double CLOSING_VOLTAGE = 0;
-    public static final double SLOW_CLOSING_VOLTAGE = 0;
+    public static final double MOVING_VOLTAGE = 0;
     public static final double POSITION_CONVERSIN_FACTOR = 0;
     public static final double VELOCITY_CONVERSIN_FACTOR = 0;
+    public static final double MAX_VELOCITY = 0;
+    public static final double MAX_ACCELERATION = 0;
     public static final int STALL_LIMIT = 0;
 }
