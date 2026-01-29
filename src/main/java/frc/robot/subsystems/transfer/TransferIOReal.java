75660925e781b7d5791dbfa879208f44881d7f5d
John <yojobama@gmail.com>
2026-01-29 17:18:21 +0200

    adding some more stuff :)

--- src/main/java/frc/robot/subsystems/transfer/TransferIOReal.java
+++ src/main/java/frc/robot/subsystems/transfer/TransferIOReal.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.transfer;
 
+import static frc.robot.subsystems.transfer.TransferConstants.*;
 import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
@@ -9,27 +11,30 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import frc.robot.POM_lib.Motors.POMSparkMax;
 
 public class TransferIOReal implements TransferIO{
-    SparkMax motor;
+    POMSparkMax motor;
+    RelativeEncoder encoder;
 
     public TransferIOReal() {
-        motor = new SparkMax(TransferConstants.TRANSFER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
+        motor = new POMSparkMax(TRANSFER_MOTOR_ID);
+
+        encoder = motor.getEncoder();
+
+        motor.setDirection(TRANSFER_MOTOR_DIRECTION);
 
         SparkMaxConfig config = new SparkMaxConfig();
-        config.idleMode(SparkBaseConfig.IdleMode.kBrake).smartCurrentLimit(50);
+        config.idleMode(SparkBaseConfig.IdleMode.kBrake).smartCurrentLimit(SMART_CURRENT_LIMIT);
 
         motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
     @Override
     public void updateInputs(TransferIOInputs inputs) {
-        inputs.velocity = motor.getEncoder().getVelocity();
+        inputs.velocity = encoder.getVelocity();
         inputs.voltage = (motor.getAppliedOutput() * motor.getBusVoltage());
     }
 
     @Override
-    public void stopMotor() {
-        setVoltage(0);
-    }
+    public void stopMotor() { setVoltage(0); }
 
     @Override
     public void setVoltage(double voltage) {
