d79ebabdb061dd58d185e67d3ac44a89c6dc97ee
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2026-02-01 20:32:45 +0200

    finished subsystem

--- src/main/java/frc/robot/subsystem/climb/climb.java
+++ src/main/java/frc/robot/subsystem/climb/climb.java
@@ -1,5 +1,24 @@
 package frc.robot.subsystem.climb;
 
-public class climb {
+import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class climb extends SubsystemBase {
+    climbIOInputsAutoLogged climbIOInputs = new climbIOInputsAutoLogged();
+    climbIO io;
+
+    public climb(climbIO io) {
+        this.io = io;
+    }
+
+    public climbIO getIO() {
+        return io;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(climbIOInputs);
+        Logger.processInputs("climb", climbIOInputs);
+    }
 }
--- src/main/java/frc/robot/subsystem/climb/climbConstants.java
+++ src/main/java/frc/robot/subsystem/climb/climbConstants.java
@@ -1,5 +1,11 @@
 package frc.robot.subsystem.climb;
 
 public class climbConstants {
-
+    public static final int MOTOR_ID = 0;
+    public static final int SERVO_CHANNEL = 0;
+    public static final int LIMIT_SWITCH_CHANNEL = 0;
+    public static final int SERVO_MAX_VOLTAGE = 6;
+    public static final int SERVO_ANGLE_RANGE = 180;
+    public static final boolean IS_NORAMLLY_OPEN = false;
+    public static final int LIMIT = 0;
 }
