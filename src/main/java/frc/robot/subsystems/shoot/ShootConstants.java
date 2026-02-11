7c53b7efaf3a4c4cc63b86f06f9dc95c0c50486e
gil-liGrota <gilgrota@gmail.com>
2026-02-11 18:37:49 +0200

    gear ratio

--- src/main/java/frc/robot/subsystems/shoot/ShootConstants.java
+++ src/main/java/frc/robot/subsystems/shoot/ShootConstants.java
@@ -7,7 +7,7 @@ public class ShootConstants {
     public static final int LEFT_HOOD_MOTOR_ID = 16; //TODO: change motor ID
     public static final int RIGHT_HOOD_MOTOR_ID = 17; //TODO: change motor ID
     public static final int FEED_MOTOR_ID = 18; //TODO: change motor ID
-    public static final double hoodGearRatio = 1.0; 
+    public static final double hoodGearRatio = 1.0 / 9.0; 
     public static final InvertedValue HOOD_DIRECTION = InvertedValue.CounterClockwise_Positive;
     public static final double slipCurrent = 30.0; //Amps
     public static final double rampRate = 0.25; //seconds from 0 to full
