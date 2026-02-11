2335ef4dcf17d09f63f18c9dbb315415837fa33d
gil-liGrota <gilgrota@gmail.com>
2026-02-11 19:12:55 +0200

    add id

--- src/main/java/frc/robot/subsystems/shoot/ShootConstants.java
+++ src/main/java/frc/robot/subsystems/shoot/ShootConstants.java
@@ -4,9 +4,9 @@ import com.ctre.phoenix6.signals.InvertedValue;
 
 public class ShootConstants {
 
-    public static final int LEFT_HOOD_MOTOR_ID = 16; //TODO: change motor ID
-    public static final int RIGHT_HOOD_MOTOR_ID = 17; //TODO: change motor ID
-    public static final int FEED_MOTOR_ID = 18; //TODO: change motor ID
+    public static final int LEFT_HOOD_MOTOR_ID = 15;
+    public static final int RIGHT_HOOD_MOTOR_ID = 16;
+    public static final int FEED_MOTOR_ID = 20;
     public static final double hoodGearRatio = 1.0 / 9.0; 
     public static final InvertedValue HOOD_DIRECTION = InvertedValue.CounterClockwise_Positive;
     public static final double slipCurrent = 30.0; //Amps
