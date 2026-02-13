a1f184c74dfbf88682967e3695e189f1ec2de9ad
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2026-02-13 10:25:42 +0200

    the commit that got deleted (need to add the arm)

--- src/main/java/frc/robot/subsystem/climb/ClimbIO.java
+++ src/main/java/frc/robot/subsystem/climb/ClimbIO.java
@@ -7,25 +7,19 @@ public interface ClimbIO {
     @AutoLog
 
     public static class ClimbIOInputs {
-        public double voltage;
-        public double output;
-        public double angle;
-        public boolean isPressed;
+        public static double voltage;
+        public static double output;
+        public static double angle;
+        public static boolean isPressed;
     }
 
     public default void updateInputs(ClimbIOInputs inputs) {
     }
 
-    public default void setClimbVoltage(double voltage) {
+    public default void setMotorVoltage(double voltage) {
     }
-    
-    public default void stopClimb() {
-    }
-    
-    public default void setArmMotor(double voltage) {
-    }
-    
-    public default void stopArm() {
+
+    public default void stopMotor() {
     }
 
     public default void setServoAngle(double angle) {
@@ -33,6 +27,20 @@ public interface ClimbIO {
 
     public default boolean isPressed() {
         return false;
+    }
+
+    public default void preClimbGoToPos(double goal) {
+    }
+
+    public default void ClimbGoToPos(double goal) {
+    }
+
+    public default boolean atGoal() {
+        return false;
+    }
+
+    public default double getPosition() {
+        return 0;
     }
 
 }
--- src/main/java/frc/robot/subsystem/climb/ClimbIOReal.java
+++ src/main/java/frc/robot/subsystem/climb/ClimbIOReal.java
@@ -1,13 +1,16 @@
 package frc.robot.subsystem.climb;
 
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.Slot1Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.revrobotics.servohub.ServoHub;
-
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
 import edu.wpi.first.wpilibj.Servo;
-import frc.robot.POM_lib.Motors.POMSparkMax;
 import frc.robot.POM_lib.sensors.POMDigitalInput;
 
 import static frc.robot.subsystem.climb.ClimbConstants.*;
@@ -15,19 +18,46 @@ import static frc.robot.subsystem.climb.ClimbConstants.*;
 public class ClimbIOReal implements ClimbIO {
     private final TalonFX motor;
     private final Servo servo;
-    private final POMSparkMax armMotor;
+    private VoltageOut voltageOut;
     private final POMDigitalInput limitSwitch;
     private final MotorOutputConfigs configs;
+    private final TalonFXConfiguration config;
+    private final PositionVoltage preClimbRequest;
+    private final PositionVoltage ClimbRequest;
+    private double goal;
 
     public ClimbIOReal() {
-        motor = new TalonFX(CLIMB_MOTOR_ID);
-        armMotor = new POMSparkMax(ARM_MOTOR_ID);
+        motor = new TalonFX(MOTOR_ID);
         servo = new Servo(SERVO_CHANNEL);
+        voltageOut = new VoltageOut(0);
         limitSwitch = new POMDigitalInput(LIMIT_SWITCH_CHANNEL, IS_NORAMLLY_OPEN);
         configs = new MotorOutputConfigs();
         configs.Inverted = InvertedValue.Clockwise_Positive;
         configs.NeutralMode = NeutralModeValue.Brake;
         motor.getConfigurator().apply(configs); // should work
+
+        config = new TalonFXConfiguration();
+        config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive; // need to check
+        config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        config.Feedback.RotorToSensorRatio = GEAR_RATIO;
+        config.TorqueCurrent.PeakForwardTorqueCurrent = CURRENT_LIMIT;
+        config.TorqueCurrent.PeakReverseTorqueCurrent = -CURRENT_LIMIT;
+        config.CurrentLimits.StatorCurrentLimit = CURRENT_LIMIT;
+        config.CurrentLimits.StatorCurrentLimitEnable = true;
+
+        Slot0Configs preClimbSlot = new Slot0Configs().withKP(Kp).withKI(Ki).withKD(Kd).withKS(Ks)
+                .withKG(BEFORE_CLIMB_Kg).withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
+        Slot1Configs ClimbSlot = new Slot1Configs().withKP(Kp).withKI(Ki).withKD(Kd).withKS(Ks).withKG(CLIMB_Kg)
+                .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
+
+        config.Slot0 = preClimbSlot;
+        config.Slot1 = ClimbSlot;
+
+        preClimbRequest = new PositionVoltage(0).withSlot(0);
+        ClimbRequest = new PositionVoltage(0).withSlot(1);
+
+        motor.getConfigurator().apply(config);
+        motor.setPosition(0);
     }
 
     @Override
@@ -39,23 +69,15 @@ public class ClimbIOReal implements ClimbIO {
     }
 
     @Override
-    public void setClimbVoltage(double voltage) {
+    public void setMotorVoltage(double voltage) {
         motor.setVoltage(voltage);
+        motor.setControl(voltageOut.withOutput(voltage));
     }
 
     @Override
-    public void stopClimb() {
-        setClimbVoltage(0);
-    }
-
-    @Override
-    public void setArmMotor(double voltage) {
-        armMotor.setVoltage(voltage);
-    }
-
-    @Override
-    public void stopArm() {
-        setArmMotor(0);
+    public void stopMotor() {
+        motor.stopMotor();
+        setMotorVoltage(0);
     }
 
     @Override
@@ -66,5 +88,25 @@ public class ClimbIOReal implements ClimbIO {
     @Override
     public boolean isPressed() {
         return limitSwitch.get();
+    }
+
+    @Override
+    public void preClimbGoToPos(double goal) {
+        motor.setControl(preClimbRequest.withPosition(goal));
+    }
+
+    @Override
+    public void ClimbGoToPos(double goal) {
+        motor.setControl(ClimbRequest.withPosition(goal));
+    }
+
+    @Override
+    public boolean atGoal() {
+        return Math.abs(getPosition() - goal) <= TOLERANCE;
+    }
+
+    @Override
+    public double getPosition() {
+        return motor.getPosition().getValueAsDouble();
     }
 }
