b2bbe6a0a5806e9fdf4d9d5d2baa9e9874a30f44
John <yojobama@gmail.com>
2026-02-15 23:15:22 +0200

    adding some more of the base of the ShootOnTheMoveCommand

--- src/main/java/frc/robot/commands/ShootOnTheMoveCommand.java
+++ src/main/java/frc/robot/commands/ShootOnTheMoveCommand.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.shoot.Shoot;
+import frc.robot.subsystems.shoot.ShootIO;
 import frc.robot.subsystems.shooterArm.ShooterArm;
 import frc.robot.util.BallisticCalculator.BallisticCalculator;
 import frc.robot.util.BallisticCalculator.BallisticCalculatorResultWithRotation;
@@ -13,17 +14,24 @@ import java.util.List;
 import java.util.function.Supplier;
 
 public class ShootOnTheMoveCommand extends Command {
-    // shooter subsystem
-    Shoot shoot;
-    ShooterArm shooterArm;
-    Supplier<Pose2d> robotPosition;
-    Pose3d targetPose;
+    // FIXME: switch to using the actual value
+    private final double FEED_SHOOT_VOLTAGE = 0;
+    private final double SHOOTER_DEFAULT_SPEED = 10.0; // [m/s]
 
-    public ShootOnTheMoveCommand(Shoot shooter, ShooterArm shooterArm, Supplier<Pose2d> shooterPosition, Pose3d targetPose) {
-        this.shoot = shooter;
-        this.shooterArm = shooterArm;
-        this.robotPosition = robotPosition;
+    // shooter subsystem
+    Shoot shooter;
+    ShooterArm shooterArm;
+    Supplier<Pose2d> shooterPosition;
+    Pose3d targetPose;
+    Supplier<Boolean> stopSupplier;
+
+    public ShootOnTheMoveCommand(Supplier<Boolean> stopSupplier, Pose3d targetPose, Shoot shooter, ShooterArm shooterArm, Supplier<Pose2d> shooterPosition) {
+        this.stopSupplier = stopSupplier;
         this.targetPose = targetPose;
+
+        this.shooter = shooter;
+        this.shooterArm = shooterArm;
+        this.shooterPosition = shooterPosition;
 
         addRequirements(shooterArm, shooter);
     }
@@ -38,8 +46,14 @@ public class ShootOnTheMoveCommand extends Command {
     }
 
     // TODO: Implement
-    BallisticCalculatorResultWithRotation chooseTrajectory(List<BallisticCalculatorResultWithRotation> results) {
-        return null;
+    BallisticCalculatorResultWithRotation chooseTrajectory(List<BallisticCalculatorResultWithRotation> results, double currentVelocity) {
+        BallisticCalculatorResultWithRotation best = null;
+        for (var result : results) {
+            if (best == null || Math.abs(currentVelocity - result.v0()) < Math.abs(currentVelocity - best.v0())) {
+                best = result;
+            }
+        }
+        return best;
     }
 
     // TODO: Implement
@@ -53,23 +67,38 @@ public class ShootOnTheMoveCommand extends Command {
     public void execute() {
         Pose2d position;
 
-        Translation3d translationToTarget = getTranslationToTarget(targetPose, robotPosition.get());
+        Translation3d translationToTarget = getTranslationToTarget(targetPose, shooterPosition.get());
 
         List<BallisticCalculatorResultWithRotation> results = BallisticCalculator.calculateForFuel(translationToTarget, null, BallisticCalculator.BallisticCalculatorMode.FAST); // FIXME: this isn't supposed to be null
 
-        BallisticCalculatorResultWithRotation best = chooseTrajectory(results);
+        ShootIO.ShootIOInputs shooterInputs = new ShootIO.ShootIOInputs();
+        shooter.getIO().updateInputs(shooterInputs);
 
+        BallisticCalculatorResultWithRotation bestTrajectory = chooseTrajectory(results, (shooterInputs.leftVelocity + shooterInputs.rightVelocity) / 2);
+        shooter.getIO().setHoodSetpoint(bestTrajectory.v0());
 
+        // TODO: see if the global angle is needed or a more detailed angle
+        shooterArm.getIO().setGoal(bestTrajectory.launchAngle());
+
+        // TODO: consider the robot's movement and see if the current rotation is good
+        if (shooter.getIO().atGoalHood() && shooterArm.getIO().atGoal()) {
+            // do all the shooting part
+            shooter.getIO().setFeedVoltage(FEED_SHOOT_VOLTAGE);
+        } else {
+            shooter.getIO().setFeedVoltage(0.0);
+        }
     }
 
     // TODO: Implement
     @Override
     public void end(boolean interrupted) {
+        shooter.getIO().stopFeed();
+        shooter.getIO().setHoodSetpoint(SHOOTER_DEFAULT_SPEED);
     }
 
     // TODO: Implement
     @Override
     public boolean isFinished() {
-        return false;
+        return stopSupplier.get();
     }
 }
