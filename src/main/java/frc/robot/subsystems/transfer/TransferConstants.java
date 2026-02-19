ee49fb0db9a45c3f2efaff73ba895b67f73fde19
gil-liGrota <gilgrota@gmail.com>
2026-02-19 16:32:08 +0200

    change current limit

--- src/main/java/frc/robot/subsystems/transfer/TransferConstants.java
+++ src/main/java/frc/robot/subsystems/transfer/TransferConstants.java
@@ -5,7 +5,7 @@ import frc.robot.POM_lib.Motors.POMMotor;
 public class TransferConstants {
     public static final int TRANSFER_MOTOR_ID = 19;
     public static final double TRANSFER_SPEED = 0.5; // TODO: replace with the actual value
-    public static final int SMART_CURRENT_LIMIT = 40;
+    public static final int SMART_CURRENT_LIMIT = 70;
     public static final boolean INVERTED = false;
 
 }
