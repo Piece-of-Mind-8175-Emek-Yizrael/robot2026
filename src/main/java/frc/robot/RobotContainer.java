6ac20ccf07e1c8f1196d6a362f919509140d8489
gil-liGrota <gilgrota@gmail.com>
2026-02-19 16:12:27 +0200

    idk why its happend

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,12 +24,12 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.POM_lib.Joysticks.PomXboxController;
-import frc.robot.commands.TransferCommands;
-import frc.robot.subsystems.transfer.Transfer;
-import frc.robot.subsystems.transfer.TransferIOReal;
 import frc.robot.commands.ShootCommands;
+import frc.robot.commands.TransferCommands;
 import frc.robot.subsystems.shoot.Shoot;
 import frc.robot.subsystems.shoot.ShootIOReal;
+import frc.robot.subsystems.transfer.Transfer;
+import frc.robot.subsystems.transfer.TransferIOReal;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
