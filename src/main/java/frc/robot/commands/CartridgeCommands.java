ac23a2bd44b505cde75c0d328eb9ce976ad60f4c
scriptedPie <yanir989898@gmail.com>
2026-03-20 14:52:01 +0200

    /

--- src/main/java/frc/robot/commands/CartridgeCommands.java
+++ src/main/java/frc/robot/commands/CartridgeCommands.java
@@ -1,5 +1,7 @@
 package frc.robot.Commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
