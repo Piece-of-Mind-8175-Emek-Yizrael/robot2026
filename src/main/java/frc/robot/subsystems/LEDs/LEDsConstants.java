da3e7fdd4eb5517a1b6bf3c3eb4456af3b9c6ba3
scriptedPie <yanir989898@gmail.com>
2026-03-24 18:40:58 +0200

    port

--- src/main/java/frc/robot/subsystems/LEDs/LEDsConstants.java
+++ src/main/java/frc/robot/subsystems/LEDs/LEDsConstants.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.LEDs;
 
 public class LEDsConstants {
-    public static final int LED_PORT = 8;
+    public static final int LED_PORT = 9;
     public static final int LENGTH = 30;
 }
