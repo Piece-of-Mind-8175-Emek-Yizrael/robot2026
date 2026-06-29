29b4086b21aa18f76b933f96de7861c89e5c2a6c
scriptedPie <yanir989898@gmail.com>
2026-06-29 23:13:27 +0300

    fix for comp

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -236,10 +236,11 @@ public class RobotContainer {
                 leds.setDefaultCommand(LEDsCommands.setAll(leds, Color.kPurple));
 
                 // driverController
+
                 swerve.setDefaultCommand(
                                 SwerveCommands.joystickDrive(swerve,
-                                                () -> driverController.getLeftY() * 0.55,
-                                                () -> driverController.getLeftX() * 0.55,
+                                                () -> driverController.getLeftY() * 0.4,
+                                                () -> driverController.getLeftX() * 0.4,
                                                 () -> driverController.getRightX() * 0.45));
 
                 driverController.LB().whileTrue(superCommands.shakeCartridge());
